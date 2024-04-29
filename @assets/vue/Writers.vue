c9febd1c3197c65eb68cd8fef5e6a85ffb3d2900
jianjian <jjaw@jjaw.cn>
2024-04-29 14:39:42 +0800

    更新贡献者显示

--- @assets/vue/Writers.vue
+++ @assets/vue/Writers.vue
@@ -1,10 +1,38 @@
 <!-- 显示文章的全部作者的组件 -->
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
 import EditInGitHub from '../../@plugin/github-edit/EditInGitHub.vue';
 import { useCommitPersons } from '../composables/ArticleInformations';
+import { getGitHubUserInfoByEmail } from '../composables/GithubUserData';
 
 
 const commitPersons = useCommitPersons();
+const users = ref<{
+  name: string;
+  email: string;
+  commits: number;
+
+  img?: string,
+  githubName?: string, //github用户名称
+  githubPage?: string // github主页链接
+  isONGithub?:boolean,//用户是否在github上存在
+}[] | undefined>(commitPersons.value?.map(({ name, email, commits }) => ({ name, email, commits })));
+
+onMounted(async () => {
+  if (users.value) {
+    for (const user of users.value) {
+      const githubUser = await getGitHubUserInfoByEmail(user.email);
+      if(githubUser){
+        user.isONGithub = true;
+        user.githubName = githubUser.githubName;
+        user.githubPage = githubUser.githubPage;
+        user.img = githubUser.img;
+      }else{
+        user.isONGithub = false;
+      }
+    }
+  }
+})
 
 </script>
 <template>
@@ -12,20 +40,22 @@ const commitPersons = useCommitPersons();
     <div class="writers-top">
       <div class="writers-title">贡献者</div>
     </div>
-    <div class="writers" v-if="commitPersons">
-      <table class="writer">
-        <tr v-for="{ name, email, commits } of commitPersons">
-          <td :title="email">{{ name }}</td>
-          <td>
-            <span>
-            <img class="timg" src="../imgs/icon/历史.svg">
-            {{ commits }}
-            </span>
-            <span>次提交</span>
-          </td>
-          <!-- <td class="email">{{ email }}</td> -->
-        </tr>
-      </table>
+    <div class="writers" v-if="users">
+      <a class="writer" :class="{canCkick:isONGithub}" v-for="{ name, email, commits ,isONGithub ,img, githubName ,githubPage } of users" :href="githubPage">
+        <div class="uimg">
+          <img v-if="img" :src="img" alt="img">
+          <span v-else>{{ (githubName || name || "O").charAt(0)  }}</span>
+        </div>
+        <div class="shwo">
+          <div class="userinfo">
+            <span class="uanme">{{ githubName || name }}</span>
+            <span class="uemail">{{ email }}</span>
+          </div>
+          <div class="commitLine">
+            <img class="timg" src="../imgs/icon/历史.svg"><span class="commit">{{ commits }}次提交</span>
+          </div>
+        </div>
+      </a>
       <EditInGitHub class="becoming-contributor" title="编辑完善此文章，成为本章贡献者。">+ 成为贡献者</EditInGitHub>
     </div>
     <div v-else>
@@ -34,16 +64,79 @@ const commitPersons = useCommitPersons();
   </div>
 </template>
 <style scoped>
-.becoming-contributor{
+.userinfo{
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+.uemail{
+  margin-left: 0.5rem;
+  font-size: 0.8rem;
+  opacity: 0.8;
+}
+.commitLine{
+  font-size: 0.8rem;
+  opacity: 0.6;
+}
+.writer > .shwo{
+  white-space: nowrap;
+  overflow: hidden;
+  max-width: calc(100% - 2rem - 0.5rem);
+}
+.uanme{
+  text-overflow: ellipsis;
+}
+
+.writer.canCkick:hover{
+  color: var(--color-time2);
+  cursor: pointer;
+}
+
+
+
+.uimg{
+  width: 2rem;
+  height: 2rem;
+  overflow: hidden;
+  border-radius: 1rem;
+  margin-right: 0.5rem;
+  background-color: var(--background-color1);
+  border: 1px solid var(--border-color);
+  position: relative;
+}
+.uimg > img{
+  width: 100%;
+  height: 100%;
+}
+.uimg > span{
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+  font-size: 1.5rem;
+}
+
+.writer{
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  align-items: center;
+  margin-bottom: 0.8rem;
+  color: var(--color);
+  text-decoration: none;
+}
+
+.becoming-contributor {
   text-decoration: none;
   display: block;
   margin-top: 0.5rem;
   font-size: 0.8rem;
   color: var(--color-time2);
 }
-.becoming-contributor:hover{
+
+.becoming-contributor:hover {
   text-decoration: underline;
 }
+
 /* .email{
   font-size: 0.8rem;
 } */
@@ -53,9 +146,6 @@ const commitPersons = useCommitPersons();
   top: 0.1em;
 }
 
-.writer > tr > td{
-  padding-right: 1rem;
-}
 
 .writers {
   padding: 0.5rem;
@@ -66,11 +156,13 @@ const commitPersons = useCommitPersons();
   overflow: hidden;
   border-radius: 0.5rem;
 }
-.writers-top{
+
+.writers-top {
   height: 1.6rem;
   background-color: var(--background-color1);
   padding: 0.5rem;
 }
+
 .writers-title {
   font-weight: bold;
 }
